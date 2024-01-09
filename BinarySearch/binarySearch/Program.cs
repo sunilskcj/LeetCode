e9158210dd3b97545132a47a9007bf526a1b6232
sunilskcj <sunilskcj@gmail.com>
2024-01-09 09:45:58 +0530

    if else

--- BinarySearch/binarySearch/Program.cs
+++ BinarySearch/binarySearch/Program.cs
@@ -5,9 +5,9 @@
         int start=0, end=nums.Length-1;
         while(start <= end){
             int mid=start+(end-start)/2;
-            if(nums[mid] == target) return mid;
-            if(nums[mid] < target) start = mid +1;
-            if(nums[mid] > target) end = mid-1;            
+            if(nums[mid] < target) {start = mid +1;}
+            else if(nums[mid] > target) {end = mid-1;}
+            else{return mid;}            
         }
         return -1;
     }
